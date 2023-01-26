f3a3009c965746cceaadce45726090ed923d51db
kri14 <63444812+kri14@users.noreply.github.com>
2023-01-27 01:29:45 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -13,7 +13,7 @@ from torchvision import transforms
 from output_utils import prepare_output
 from PIL import Image
 
-data_dir = 'C:/Users/91991/Downloads/stream/'
+data_dir = 'Recipe-Generation-from-Food-Image/Foodimg2Ing/data/'
 ngrs_vocab = pickle.load(open(os.path.join(data_dir, 'ingr_vocab.pkl'), 'rb'))
 vocab = pickle.load(open(os.path.join(data_dir, 'instr_vocab.pkl'), 'rb'))
 ingr_vocab_size = len(ngrs_vocab)
@@ -94,7 +94,7 @@ def predict(model,image):
 
     
 def main():
-    image = Image.open("C:/Users/91991/Downloads/stream/data/food_header.jpg").resize((680, 150))
+    image = Image.open("Recipe-Generation-from-Food-Image/Foodimg2Ing/data/food_header.jpg").resize((680, 150))
     st.image(image)
     st.markdown('<div style="text-align: center;"><h1>PIC\'N BASKET</h1><div>',unsafe_allow_html=True)
     st.text("")
@@ -118,7 +118,7 @@ def main():
         st.markdown('<div style="text-align: center;">Kriti(Co19335)<br>Karan(Co19332)<div>',unsafe_allow_html=True)
         st.markdown('<div style="text-align: center;"><i>Chandigarh College of Engineering & Technology</i><br><br><div>',unsafe_allow_html=True)        
         col1, col2, col3 = st.columns([1,3,1])
-        image = Image.open("C:/Users/91991/Downloads/stream/data/logo.png").resize((200, 90))
+        image = Image.open("Recipe-Generation-from-Food-Image/Foodimg2Ing/data/logo.png").resize((200, 90))
         with col1:
             st.write('')
         with col2:
